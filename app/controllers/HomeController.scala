7be4cff2f1c90ba1c4fb6c3168339ae7e1b97d7c
Konstantin Nesterenko <kostya.nesterenko@gmail.com>
2019-01-11 21:20:54 +0200

    in memory list

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import models.Message
+import play.api.data._
 import javax.inject._
 import play.api._
 import play.api.mvc._
@@ -9,7 +11,20 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+
+  import MessageForm._
+
+  private val messages = scala.collection.mutable.ArrayBuffer(
+    Message("Cras justo odio"),
+    Message("Dapibus ac facilisis in"),
+    Message("Morbi leo risus")
+  )
+
+  // The URL to the widget.  You can call this directly from the template, but it
+  // can be more convenient to leave the template completely stateless i.e. all
+  // of the "WidgetController" references are inside the .scala file.
+  private val postUrl = routes.HomeController.createMessage()
 
   /**
    * Create an Action to render an HTML page.
@@ -18,7 +33,31 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index() = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.index(form, postUrl))
+  }
+
+  def list() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.list(messages))
+  }
+
+  // This will be the action that handles our form post
+  def createMessage = Action { implicit request: MessagesRequest[AnyContent] =>
+    val errorFunction = { formWithErrors: Form[Data] =>
+      // This is the bad case, where the form had validation errors.
+      // Let's show the user the form again, with the errors highlighted.
+      // Note how we pass the form with errors to the template.
+      BadRequest(views.html.index(formWithErrors, postUrl))
+    }
+
+    val successFunction = { data: Data =>
+      // This is the good case, where the form was successfully parsed as a Data object.
+      val message = Message(text = data.text)
+      messages.append(message)
+      Redirect(routes.HomeController.list()).flashing("info" -> "Widget added!")
+    }
+
+    val formValidationResult = form.bindFromRequest
+    formValidationResult.fold(errorFunction, successFunction)
   }
 }
